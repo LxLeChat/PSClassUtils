212cddc959742094bc1053b9ffdcebf3bb198917
LxLeChat <yanaski@msn.com>
2019-06-28 22:13:26 +0200

    az

--- CI/02_Install.ps1
+++ CI/02_Install.ps1
@@ -1,11 +1,11 @@
 #Install-PackageProvider -Name Chocolatey -force;
 #Register-PackageSource -Name Chocolatey -ProviderName Chocolatey -Location http://chocolatey.org/api/v2/ -Force -Trusted;
 Install-Package -name GraphViz -ForceBootstrap -Force;
-dir 'C:\program files\*' -erroraction silentlycontinue
-dir 'C:\program files (x86)\*' -erroraction silentlycontinue
+get-childitem 'C:\program files\PackageManagement\'
+get-childitem 'C:\program files (x86)\NuGet\'
 #Start-Sleep -Seconds 2;
 
 #Install-PackageProvider -Name NuGet -Force -Scope "CurrentUser";
-Install-Module -Name PSGraph -Force -verbose -Scope "CurrentUser" -AllowClobber;
-Install-Module -Name PSScriptAnalyzer -Force -Scope "CurrentUser";
-Install-Module -Name Pester -Force -verbose -Scope "CurrentUser";
+#Install-Module -Name PSGraph -Force -verbose -Scope "CurrentUser" -AllowClobber;
+#Install-Module -Name PSScriptAnalyzer -Force -Scope "CurrentUser";
+#Install-Module -Name Pester -Force -verbose -Scope "CurrentUser";
