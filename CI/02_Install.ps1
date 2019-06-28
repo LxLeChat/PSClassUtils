3d52f740df0d0451e46c8e0a856e357d8ec7ffcd
LxLeChat <yanaski@msn.com>
2019-06-28 22:36:42 +0200

    Update 02_Install.ps1

--- CI/02_Install.ps1
+++ CI/02_Install.ps1
@@ -1,11 +1,8 @@
 #Install-PackageProvider -Name Chocolatey -force;
 #Register-PackageSource -Name Chocolatey -ProviderName Chocolatey -Location http://chocolatey.org/api/v2/ -Force -Trusted;
-Install-Package -name GraphViz -ForceBootstrap -Force;
-#get-childitem 'C:\program files\PackageManagement\*' -directory -Recurse | select fullname
-#get-childitem 'C:\program files\PackageManagement\Nuget\*' -directory -Recurse | select fullname
-#get-childitem 'C:\program files (x86)\NuGet\*' -directory -Recurse | select fullname
-
-get-childitem 'C:\program files\PackageManagement\NuGet\Packages\Graphviz.2.38.0.2' -Recurse -File | select fullname 
+#"C:\program files\PackageManagement\NuGet\Packages\Graphviz.2.38.0.2\dot.exe"
+Install-Package -name GraphViz -ForceBootstrap -Force -Destination "C:\Program Files\NuGet\Packages\Graphviz";
+get-childitem 'C:\program files\PackageManagement\NuGet\Packages\Graphviz' -Recurse -File | select fullname 
 #Start-Sleep -Seconds 2;
 
 #Install-PackageProvider -Name NuGet -Force -Scope "CurrentUser";
