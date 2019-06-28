39c9ea12a809790432d1055b19600b1bd168e3f1
LxLeChat <yanaski@msn.com>
2019-06-28 22:41:43 +0200

    here we go again

--- CI/02_Install.ps1
+++ CI/02_Install.ps1
@@ -1,11 +1,11 @@
-#Install-PackageProvider -Name Chocolatey -force;
-#Register-PackageSource -Name Chocolatey -ProviderName Chocolatey -Location http://chocolatey.org/api/v2/ -Force -Trusted;
+## In 2019 image, this is the default path
 #"C:\program files\PackageManagement\NuGet\Packages\Graphviz.2.38.0.2\dot.exe"
-Install-Package -name GraphViz -ForceBootstrap -Force -Destination "C:\Program Files\NuGet\Packages\Graphviz";
-get-childitem 'C:\Program Files\NuGet\Packages\Graphviz' -Recurse -File | select fullname 
-#Start-Sleep -Seconds 2;
 
-#Install-PackageProvider -Name NuGet -Force -Scope "CurrentUser";
-#Install-Module -Name PSGraph -Force -verbose -Scope "CurrentUser" -AllowClobber;
-#Install-Module -Name PSScriptAnalyzer -Force -Scope "CurrentUser";
-#Install-Module -Name Pester -Force -verbose -Scope "CurrentUser";
+## Force Path .. otherwise psgraph wont find GraphViz binaries !
+Install-Package -name GraphViz -ForceBootstrap -Force -Destination "C:\Program Files\NuGet\Packages";
+
+Start-Sleep -Seconds 2;
+
+Install-Module -Name PSGraph -Force -verbose -Scope "CurrentUser" -AllowClobber;
+Install-Module -Name PSScriptAnalyzer -Force -Scope "CurrentUser";
+Install-Module -Name Pester -Force -verbose -Scope "CurrentUser";
