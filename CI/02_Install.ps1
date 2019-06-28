93e49e8570ac5068bc9c3058a1ad54c8ef77cf78
LxLeChat <yanaski@msn.com>
2019-06-28 22:02:49 +0200

    az

--- CI/02_Install.ps1
+++ CI/02_Install.ps1
@@ -1,6 +1,8 @@
 #Install-PackageProvider -Name Chocolatey -force;
 #Register-PackageSource -Name Chocolatey -ProviderName Chocolatey -Location http://chocolatey.org/api/v2/ -Force -Trusted;
-Find-Package graphviz | Install-Package -ForceBootstrap -Force;
+Install-Package -name GraphViz -ForceBootstrap -Force;
+dir 'C:\Program Files\NuGet\Packages' -erroraction silentlycontinue
+dir 'C:\program files*' -erroraction silentlycontinue
 #Start-Sleep -Seconds 2;
 
 #Install-PackageProvider -Name NuGet -Force -Scope "CurrentUser";
